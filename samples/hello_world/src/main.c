48a84ad13ede5d0ca2591c3cddfcd89626f3ed12
Stephanos Ioannidis <root@stephanos.io>
2022-08-24 17:43:00 +0900

    [TEST] GCC 12 weak symbol GOT indirection demo

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -6,7 +6,15 @@
 
 #include <zephyr/zephyr.h>
 
+extern __weak void test(int a, int b);
+
 void main(void)
 {
 	printk("Hello World! %s\n", CONFIG_BOARD);
+
+	if (test != NULL) {
+		test(1, 2);
+	}
+
+	printk("Done\n");
 }
---
+++ samples/hello_world/src/test.c
@@ -0,0 +1,8 @@
+#include <zephyr/zephyr.h>
+
+#if 1
+void test(int a, int b)
+{
+	printk("Hello from test(%d, %d)\n", a, b);
+}
+#endif
